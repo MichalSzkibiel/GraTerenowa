5141b2e9938c0bd4c1f9c5ea227d23e1a5c6d416
karoluss96 <33833354+karoluss96@users.noreply.github.com>
2018-04-09 15:28:06 +0200

    Update
    Dodanie plików do Desctopowej wersji

--- app/src/main/java/com/graterenowa/graterenowa/GameFeature.java
+++ app/src/main/java/com/graterenowa/graterenowa/GameFeature.java
@@ -10,23 +10,10 @@ public class GameFeature {
     public LatLng pos;
     public String name;
     public String quest;
-    public int commissions;
-    public boolean found;
 
     public GameFeature(LatLng position, String nm, String question){
         pos = position;
-        nm = nm.replace("_", " ");
-        nm = nm.replace("$", ",");
         name = nm;
-        question = question.replace("_", " ");
-        question = question.replace("$", ",");
         quest = question;
-        commissions = 0;
-        found = false;
-    }
-
-    @Override
-    public String toString(){
-        return "{Position:" + pos.toString() + ", Name:" + name + ", Quest:" + quest;
     }
 }
--- app/src/main/java/com/graterenowa/graterenowa/GameSetupActivity.java
+++ app/src/main/java/com/graterenowa/graterenowa/GameSetupActivity.java
@@ -1,17 +1,12 @@
 package com.graterenowa.graterenowa;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,31 +15,32 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polygon;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.List;
-
-public class GameSetupActivity extends AppCompatActivity implements OnMapReadyCallback {
+//Okienko wyboru zestawu gry terenowej, moja działka
+public class GameSetupActivity extends FragmentActivity {
 
     private GoogleMap mMap;
     private Spinner chooseSpinner;
-    public static FeaturesContainer current;
-    private Polygon range_of_game;
+    private FeaturesContainer current;
     public static String TAG = "My";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_setup);
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.setupMap);
-        mapFragment.getMapAsync(this);
-        Log.d(TAG, mapFragment.toString());
+        MapFragment mapFragment = MapFragment.newInstance();
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+                LatLng pos = new LatLng(52.25, 21.0);
+                mMap.moveCamera(CameraUpdateFactory.zoomBy(15));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));
+            }
+        });
         chooseSpinner = findViewById(R.id.setChoose);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.game_sets, android.R.layout.simple_spinner_item);
@@ -55,82 +51,22 @@ public class GameSetupActivity extends AppCompatActivity implements OnMapReadyCa
         chooseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,
-                int pos, long id) {
+                                       int pos, long id) {
                 // An item was selected. You can retrieve the selected item using
-                if (range_of_game != null)
-                    range_of_game.remove();
                 String json = "";
-                String name = "";
                 switch (parent.getItemAtPosition(pos).toString()) {
                     case "Politechnika Warszawska":
                         return;
                     case "Pole Mokotowskie":
                         json = getResources().getString(R.string.Pole_Mokotowskie_set);
-                        name = "Pole Mokotowskie";
                 }
-                current = new FeaturesContainer(name, json);
-                range_of_game = mMap.addPolygon(current.range);
-                List<LatLng> points = range_of_game.getPoints();
-                LatLngBounds bounds = getBoundsFromPolygon(points);
-                mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 20));
+                current = new FeaturesContainer(json);
+                mMap.addPolygon(current.range);
 
             }
             public void onNothingSelected(AdapterView<?> parent) {
-               // Another interface callback
+                // Another interface callback
             }
         });
-        Button beginGame = (Button) findViewById(R.id.BeginGameButton);
-        beginGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (current == null){
-                    createDialog();
-                    return;
-                }
-                makeIntent();
-            }
-        });
-    }
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        LatLng pos = new LatLng(52.25, 21.0);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, 10));
-    }
-
-    public static LatLngBounds getBoundsFromPolygon(List<LatLng> points){
-        double latn = points.get(0).latitude;
-        double lats = points.get(0).latitude;
-        double lone = points.get(0).longitude;
-        double lonw = points.get(0).longitude;
-        for (int i = 1; i < points.size(); ++i){
-            if (points.get(i).latitude > latn)
-                latn = points.get(i).latitude;
-            else if (points.get(i).latitude < lats)
-                lats = points.get(i).latitude;
-            if (points.get(i).longitude > lone)
-                lone = points.get(i).longitude;
-            else if (points.get(i).longitude < lonw)
-                lonw = points.get(i).longitude;
-        }
-        LatLng ne = new LatLng(latn, lone);
-        LatLng sw = new LatLng(lats, lonw);
-        return new LatLngBounds(sw, ne);
-    }
-
-    private void makeIntent(){
-        Intent startGame = new Intent(this, MapsActivity.class);
-        startActivity(startGame);
-        finish();
-    }
-    private void createDialog(){
-        new AlertDialog.Builder(this)
-                .setMessage("Nie wybrałeś jeszcze zestawu.")
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-        }).show();
     }
 }
