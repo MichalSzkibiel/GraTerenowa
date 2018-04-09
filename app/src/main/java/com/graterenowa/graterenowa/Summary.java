a7d9c8f4c5b4712559b6bbcbe6caa14fe0cad0c2
karoluss96 <33833354+karoluss96@users.noreply.github.com>
2018-04-09 16:16:44 +0200

    Koniec i powrot
    Dodanie funkcji obsługującej przycisk kończący grę i wracającego do głównego menu!

--- app/src/main/java/com/graterenowa/graterenowa/Summary.java
+++ app/src/main/java/com/graterenowa/graterenowa/Summary.java
@@ -3,6 +3,8 @@ package com.graterenowa.graterenowa;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 //Podsumowanie, twoja działka Karol
@@ -18,5 +20,14 @@ public class Summary extends AppCompatActivity {
         String ans="Gratulacje, w grze zdobyles"+ points + "punktow!";
         score=(TextView)findViewById(R.id.twojepodsumowanie);
         score.setText(ans);
+
+        Button KiP = (Button) findViewById(R.id.register);
+        KiP.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                finish();//Koniec i powrot do glownego menu
+            }
+        });
     }
 }
